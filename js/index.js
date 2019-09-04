8532bde07306f545c67f4195e31f5a0a6e789e96
Dan Levy <dlevy1@kaleidahealth.org>
2019-09-04 19:03:52 -0400

    Website setup.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* =============================
+ * File Path: js/index.js
+ * Description: Script for home page
+ * Author: Dan Levy
+ * Email: danlevy124@gmail.com
+ * Created Date: 9/4/2019
+============================= */
