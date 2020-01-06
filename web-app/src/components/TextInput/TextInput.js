22c7136860be8867448a0b0e0b2049ddb0228ac1
Dan Levy <danlevy124@gmail.com>
2020-01-05 23:27:18 -0500

    Added startup page. Added sign up page (the tablet and mobile views still need to be added).

---
+++ web-app/src/components/TextInput/TextInput.js
@@ -0,0 +1,44 @@
+// ----------------------------------------------------------------------------
+// File Path: src/components/TextInput/TextInput.js
+// Description: Renders the text input component
+// Author: Dan Levy
+// Email: danlevy124@gmail.com
+// Created Date: 1/5/2020
+// ----------------------------------------------------------------------------
+
+import React from "react";
+import styles from "./TextInput.module.scss";
+
+const TextInput = props => {
+    const inputClassNames = [styles.input];
+    if (props.value.length > 0) {
+        inputClassNames.push(styles.inputValid);
+    }
+
+    const inputElement = (
+        <input
+            className={inputClassNames.join(" ")}
+            type={props.inputType}
+            name={props.inputName}
+            value={props.value}
+            onChange={props.onChange}
+            required={props.isRequired}
+        />
+    );
+
+    const inputContainerClassNames = [styles.inputContainer];
+    if (props.addBottomMargin) {
+        inputContainerClassNames.push(styles.inputContainerMarginBottom);
+    }
+
+    return (
+        <div className={inputContainerClassNames.join(" ")}>
+            {inputElement}
+            <label className={styles.label} htmlFor={props.inputName}>
+                <span className={styles.content}>{props.labelText}</span>
+            </label>
+        </div>
+    );
+};
+
+export default TextInput;
--- web-app/src/pages/Auth/Auth.js
+++ web-app/src/pages/Auth/Auth.js
@@ -8,11 +8,113 @@
 
 import React, { Component } from "react";
 import * as actions from "../../store/actions";
+import TextInput from "../../components/TextInput/TextInput";
+import RectangularButton from "../../components/RectangularButton/RectangularButton";
+import TextButton from "../../components/TextButton/TextButton";
 import styles from "./Auth.module.scss";
+import logo from "../../assets/logos/music-assistant-logo.png";
 
 class Auth extends Component {
+    state = {
+        formElements: [
+            {
+                inputType: "email",
+                inputName: "email",
+                labelText: "Email",
+                value: "",
+                isRequired: true,
+                addBottomMargin: true
+            },
+            {
+                inputType: "password",
+                inputName: "password",
+                labelText: "Password",
+                value: "",
+                isRequired: true,
+                classNames: [],
+                addBottomMargin: true
+            }
+        ]
+    };
+
+    inputValueChangedHandler = (event, index) => {
+        const value = event.target.value;
+        this.setState(prevState => {
+            const updatedFormElements = [...prevState.formElements];
+            const updatedInput = { ...prevState.formElements[index] };
+            updatedInput.value = value;
+            updatedFormElements[index] = updatedInput;
+            return {
+                formElements: updatedFormElements
+            };
+        });
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+        console.log(this.state.formElements);
+    };
+
     render() {
-        return <div className={styles.authContainer}></div>;
+        const formElements = this.state.formElements.map((formElement, index) => {
+            return (
+                <TextInput
+                    key={index}
+                    inputType={formElement.inputType}
+                    inputName={formElement.inputName}
+                    labelText={formElement.labelText}
+                    value={formElement.value}
+                    isRequired={formElement.isRequired}
+                    classNames={formElement.classNames}
+                    addBottomMargin={formElement.addBottomMargin}
+                    onChange={event => this.inputValueChangedHandler(event, index)}
+                />
+            );
+        });
+
+        return (
+            <div className={styles.auth}>
+                <div className={styles.authContainer}>
+                    <div className={styles.authInfo}>
+                        <img
+                            className={styles.authInfoLogo}
+                            src={logo}
+                            alt='Music Assistant Logo'
+                        />
+                        <h1 className={styles.authInfoHeading}>
+                            Welcome to
+                            <br />
+                            The Music Assistant
+                        </h1>
+                        <h2 className={styles.authInfoSubheading}>
+                            Your gateway to better singing
+                        </h2>
+                    </div>
+                    <div className={styles.authCard}>
+                        <h3 className={styles.authCardHeading}>Sign Up</h3>
+                        <form className={styles.authCardForm} onSubmit={this.submitHandler}>
+                            {formElements}
+                            <RectangularButton
+                                type='submit'
+                                value='submit'
+                                text='Sign Up'
+                                backgroundColor='green'
+                                center='true'
+                            />
+                        </form>
+                        <div className={styles.authCardChangeAuth}>
+                            <TextButton
+                                type='button'
+                                value='change-auth'
+                                text='Already have an account?'
+                                textColor='blue'
+                                center='false'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
