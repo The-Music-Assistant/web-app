daa2a3cf521435a26b2a0b48706c9f4fb860fb3f
dgriessler <dgriessler2016@my.fit.edu>
2020-02-23 21:12:21 -0500

    Fixed pitch starting for treble and bass on exercise

--- web-app/src/components/PracticeMain/PracticeSheetMusic/TexLoaded.js
+++ web-app/src/components/PracticeMain/PracticeSheetMusic/TexLoaded.js
@@ -24,6 +24,14 @@ class TexLoaded {
         this.typeOfTex = typeOfTex;
         this.partNames = partNames;
         this.clefs = clefs;
+        if (this.clefs) {
+            this.clefs.forEach((track) => {
+                for (let i = 0; i < track.length; i++) {
+                    track[i] = track[i].toLowerCase();
+                }
+                this.mutedTracks.push(false);
+            });
+        }
         this.myPart = myPart;
         this.currentTrackIndexes = [0];
         this.firstBarMeasurePosition = null;
