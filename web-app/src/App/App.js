8ae040e62b65c49718821ab8cf958e29aa60f234
Dan Levy <danlevy124@gmail.com>
2020-03-14 16:53:06 -0400

    SideNav component is now fixed with a max height of 100vh.

--- web-app/src/App/App.js
+++ web-app/src/App/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <div className='App'>
+                <div className='app'>
                     {this.getRedirect()}
                     <Switch>
                         <Route path='/startup'>
