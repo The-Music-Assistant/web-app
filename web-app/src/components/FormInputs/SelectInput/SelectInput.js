b6efce73de299d01c983ec5bec385a0e62ca729d
Dan Levy <danlevy124@gmail.com>
2020-03-15 14:52:06 -0400

    Added transition to SelectInput component's options (show/hide). The input's value and selector button is now updated based on the selected option. The selector button needs to stay the same width when the value changes though (not yet fixed).

--- web-app/src/components/FormInputs/SelectInput/SelectInput.js
+++ web-app/src/components/FormInputs/SelectInput/SelectInput.js
@@ -7,7 +7,7 @@
 // ----------------------------------------------------------------------------
 
 // NPM module imports
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 // File imports
@@ -18,31 +18,78 @@ import downArrowWhite from "../../../assets/icons/down-arrow-white.svg";
 // Style imports
 import styles from "./SelectInput.module.scss";
 
-const SelectInput = props => {
-    const getOptions = () => {
-        return props.options.map(optionName => {
-            return <h2 className={styles.selectInputOption}>{optionName}</h2>;
+class SelectInput extends Component {
+    // Component state
+    state = {
+        showDropdown: false,
+        dropdownValue: this.props.placeholder
+    };
+
+    selectorButtonClickedHandler = () => {
+        this.setState(prevState => {
+            return {
+                showDropdown: !prevState.showDropdown
+            };
         });
     };
 
-    // Returns the JSX to display
-    return (
-        <div className={styles.selectInput}>
-            <input className={styles.selectInputInputElement} type='text' name={props.name} />
-            <div className={`${styles.selectInputSelector} ${styles[props.color]}`}>
-                <h2 className={styles.selectInputSelectorTitle}>{props.placeholder}</h2>
-                <img
-                    className={styles.selectInputSelectorArrowImg}
-                    src={props.color === colorOptions.WHITE ? downArrowBlue : downArrowWhite}
-                    alt={"Arrow"}
+    optionButtonClickedHandler = event => {
+        this.setState({ dropdownValue: event.target.value });
+    };
+
+    getOptions = () => {
+        return this.props.options.map(optionName => {
+            return (
+                <button
+                    className={`${styles.selectInputOption} ${styles[this.props.color]}`}
+                    type='button'
+                    value={optionName}
+                    onClick={this.optionButtonClickedHandler}>
+                    {optionName}
+                </button>
+            );
+        });
+    };
+
+    getOptionsClassList = () => {
+        let classList = `${styles.selectInputOptions} ${styles[this.props.color]}`;
+        if (this.state.showDropdown) {
+            classList += ` ${styles.selectInputOptionsShow}`;
+        } else {
+            classList += ` ${styles.selectInputOptionsHide}`;
+        }
+
+        return classList;
+    };
+
+    render() {
+        // Returns the JSX to display
+        return (
+            <div className={styles.selectInput}>
+                <input
+                    className={styles.selectInputInputElement}
+                    type='text'
+                    name={this.props.name}
+                    value={this.state.dropdownValue}
                 />
+                <button
+                    className={`${styles.selectInputSelector} ${styles[this.props.color]}`}
+                    type='button'
+                    onClick={this.selectorButtonClickedHandler}>
+                    <h2 className={styles.selectInputSelectorTitle}>{this.state.dropdownValue}</h2>
+                    <img
+                        className={styles.selectInputSelectorArrowImg}
+                        src={
+                            this.props.color === colorOptions.WHITE ? downArrowBlue : downArrowWhite
+                        }
+                        alt={"Arrow"}
+                    />
+                </button>
+                <div className={this.getOptionsClassList()}>{this.getOptions()}</div>
             </div>
-            <div className={`${styles.selectInputOptions} ${styles[props.color]}`}>
-                {getOptions()}
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 // Prop types for the SelectInput component
 SelectInput.propTypes = {
