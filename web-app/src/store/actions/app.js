e7e80a588036ff416d8eaa735cdc811b160171f6
Dan Levy <danlevy124@gmail.com>
2020-04-14 18:26:47 -0400

    Began commenting the store folder.

--- web-app/src/store/actions/app.js
+++ web-app/src/store/actions/app.js
@@ -1,15 +1,26 @@
-/* ----------------------------------------------------------------------------
-// File Path: src/store/actions/app.js
-// Description: App Redux actions
-// Author: Dan Levy
-// Email: danlevy124@gmail.com
-// Created Date: 3/23/2020
----------------------------------------------------------------------------- */
-
 // File imports
 import * as actionTypes from "./actionTypes";
 
+/**
+ * Redux app actions
+ * @module reduxAppActions
+ * @category Redux
+ * @author Dan Levy <danlevy124@gmail.com>
+ */
+
+/**
+ * Sets the browser type
+ * @function
+ * @param {boolean} isMobileBrowser - Indicates if the browser is a mobile browser
+ * @returns {SetBrowserTypeReturnObject}
+ */
 export const setBrowserType = (isMobileBrowser) => {
+    /**
+     * Return object for the setBrowserType function
+     * @typedef {object} SetBrowserTypeReturnObject
+     * @property {module:reduxActionTypes} type - An action type
+     * @property {boolean} isMobileBrowser - Indicates if the browser is a mobile browser
+     */
     return {
         type: actionTypes.IS_MOBILE_BROWSER,
         isMobileBrowser,
