ca53850385ea144073efdbc04eb6516992829556
Dan Levy <danlevy124@gmail.com>
2020-04-17 14:46:36 -0400

    Added milestone 6 files.

---
+++ files/web-app-docs/entry.js
@@ -0,0 +1,146 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../../src/components/AlertBar/AlertBar.js';
+reactComponents['AlertBar'] = Component0;
+
+import Component1 from '../../src/App/App.js';
+reactComponents['App'] = Component1;
+
+import Component2 from '../../src/pages/Auth/Auth.js';
+reactComponents['Auth'] = Component2;
+
+import Component3 from '../../src/components/AuthCards/AuthCard/AuthCard.js';
+reactComponents['AuthCard'] = Component3;
+
+import Component4 from '../../src/components/Buttons/BackButton/BackButton.js';
+reactComponents['BackButton'] = Component4;
+
+import Component5 from '../../src/components/Buttons/ButtonContainer/ButtonContainer.js';
+reactComponents['ButtonContainer'] = Component5;
+
+import Component6 from '../../src/components/ChoirSelection/ChoirCards/ChoirCard/ChoirCard.js';
+reactComponents['ChoirCard'] = Component6;
+
+import Component7 from '../../src/components/ChoirMembers/ChoirMembers.js';
+reactComponents['ChoirMembers'] = Component7;
+
+import Component8 from '../../src/components/ChoirSelection/ChoirCards/ChoirOptionCard/ChoirOptionCard.js';
+reactComponents['ChoirOptionCard'] = Component8;
+
+import Component9 from '../../src/components/ChoirSelection/ChoirSelection.js';
+reactComponents['ChoirSelection'] = Component9;
+
+import Component10 from '../../src/components/Music/MusicPerformanceHeader/ExerciseGenerator/ExerciseGenerator.js';
+reactComponents['ExerciseGenerator'] = Component10;
+
+import Component11 from '../../src/components/Footer/Footer.js';
+reactComponents['Footer'] = Component11;
+
+import Component12 from '../../src/components/Header/HamburgerMenu/HamburgerMenu.js';
+reactComponents['HamburgerMenu'] = Component12;
+
+import Component13 from '../../src/components/Header/Header.js';
+reactComponents['Header'] = Component13;
+
+import Component14 from '../../src/components/Home/Home.js';
+reactComponents['Home'] = Component14;
+
+import Component15 from '../../src/components/FormInputs/ImageInput/ImageInput.js';
+reactComponents['ImageInput'] = Component15;
+
+import Component16 from '../../src/components/FormInputs/TextInputs/LargeTextInput/LargeTextInput.js';
+reactComponents['LargeTextInput'] = Component16;
+
+import Component17 from '../../src/components/Spinners/LoadingContainer/LoadingContainer.js';
+reactComponents['LoadingContainer'] = Component17;
+
+import Component18 from '../../src/components/Spinners/LoadingHUD/LoadingHUD.js';
+reactComponents['LoadingHUD'] = Component18;
+
+import Component19 from '../../src/components/ChoirMembers/MemberCard/MemberCard.js';
+reactComponents['MemberCard'] = Component19;
+
+import Component20 from '../../src/components/MobileNav/MobileNav.js';
+reactComponents['MobileNav'] = Component20;
+
+import Component21 from '../../src/components/MobileNav/MobileNavLink/MobileNavLink.js';
+reactComponents['MobileNavLink'] = Component21;
+
+import Component22 from '../../src/components/Music/Music.js';
+reactComponents['Music'] = Component22;
+
+import Component23 from '../../src/components/MusicSelection/MusicCard/MusicCard.js';
+reactComponents['MusicCard'] = Component23;
+
+import Component24 from '../../src/components/Music/PracticeMusicHeader/MusicControls/MusicControls.js';
+reactComponents['MusicControls'] = Component24;
+
+import Component25 from '../../src/components/Music/MusicPerformanceHeader/MusicPerformanceHeader.js';
+reactComponents['MusicPerformanceHeader'] = Component25;
+
+import Component26 from '../../src/components/MusicSelection/MusicSelection.js';
+reactComponents['MusicSelection'] = Component26;
+
+import Component27 from '../../src/components/PageHeader/PageHeader.js';
+reactComponents['PageHeader'] = Component27;
+
+import Component28 from '../../src/components/Music/PracticeMusicHeader/PracticeMusicHeader.js';
+reactComponents['PracticeMusicHeader'] = Component28;
+
+import Component29 from '../../src/pages/Primary/Primary.js';
+reactComponents['Primary'] = Component29;
+
+import Component30 from '../../src/components/AuthCards/ProfileCard/ProfileCard.js';
+reactComponents['ProfileCard'] = Component30;
+
+import Component31 from '../../src/components/Progress/Progress.js';
+reactComponents['Progress'] = Component31;
+
+import Component32 from '../../src/components/Buttons/RectangularButton/RectangularButton.js';
+reactComponents['RectangularButton'] = Component32;
+
+import Component33 from '../../src/components/FormInputs/SelectInput/SelectInput.js';
+reactComponents['SelectInput'] = Component33;
+
+import Component34 from '../../src/components/SideNav/SideNav.js';
+reactComponents['SideNav'] = Component34;
+
+import Component35 from '../../src/components/SideNav/SideNavLink/SideNavLink.js';
+reactComponents['SideNavLink'] = Component35;
+
+import Component36 from '../../src/components/FormInputs/TextInputs/SmallTextInput/SmallTextInput.js';
+reactComponents['SmallTextInput'] = Component36;
+
+import Component37 from '../../src/pages/Startup/Startup.js';
+reactComponents['Startup'] = Component37;
+
+import Component38 from '../../src/components/Buttons/TextButton/TextButton.js';
+reactComponents['TextButton'] = Component38;
+
+import Component39 from '../../src/components/Header/UserWidget/UserWidget.js';
+reactComponents['UserWidget'] = Component39;
+
+import Component40 from '../../src/pages/Welcome/Welcome.js';
+reactComponents['Welcome'] = Component40;
