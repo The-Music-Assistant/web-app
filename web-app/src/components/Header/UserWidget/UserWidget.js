ac2f705e25ff0a5cff2de3788eed5c8ab33ccd0a
Dan Levy <danlevy124@gmail.com>
2020-05-14 18:52:11 -0400

    Converted the UserWidget to Use React Hooks

--- web-app/src/components/Header/UserWidget/UserWidget.js
+++ web-app/src/components/Header/UserWidget/UserWidget.js
@@ -1,84 +1,61 @@
 // NPM module imports
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 // Style imports
 import styles from "./UserWidget.module.scss";
 
 /**
  * Renders the UserWidget component
- * @extends {Component}
  * @component
  * @category Header
  * @author Dan Levy <danlevy124@gmail.com>
  */
-class UserWidget extends Component {
+const UserWidget = () => {
     /**
-     * UserWidget component state
-     * @property {boolean} isProfilePictureError - Indicates if there was an error displaying the user's profile picture
+     * Indicates if there was an error displaying the user's profile picture
+     * {[isProfilePictureError, setIsProfilePictureError]: []}
      */
-    state = {
-        isProfilePictureError: false,
-    };
+    const [isProfilePictureError, setIsProfilePictureError] = useState(false);
+
+    /**
+     * The user's name
+     * @type {string}
+     */
+    const name = useSelector((state) => state.auth.usersName);
+
+    /**
+     * The user's profile picture url
+     * @type {string}
+     */
+    const profilePictureUrl = useSelector(
+        (state) => state.auth.usersPictureUrl
+    );
 
     /**
      * Updates state indicating that there was a profile picture error
-     * @function
      */
-    profilePictureErrorHandler = () => {
-        this.setState({ isProfilePictureError: true });
-    };
+    const profilePictureErrorHandler = () => setIsProfilePictureError(true);
 
     /**
      * Renders the UserWidget component
      */
-    render() {
-        return (
-            <section className={styles.userWidget}>
-                {/* User's profile picture */}
-                {this.props.profilePictureUrl &&
-                !this.state.isProfilePictureError ? (
-                    <img
-                        className={styles.userWidgetImg}
-                        src={this.props.profilePictureUrl}
-                        alt="Profile Pic"
-                        onError={this.profilePictureErrorHandler}
-                    />
-                ) : null}
+    return (
+        <section className={styles.userWidget}>
+            {/* User's profile picture */}
+            {profilePictureUrl && !isProfilePictureError ? (
+                <img
+                    className={styles.userWidgetImg}
+                    src={profilePictureUrl}
+                    alt="Profile Pic"
+                    onError={profilePictureErrorHandler}
+                />
+            ) : null}
 
-                {/* User's name */}
-                <h2 className={styles.userWidgetName}>{this.props.name}</h2>
-            </section>
-        );
-    }
-}
-
-/**
- * Gets the current state from Redux and passes parts of it to the UserWidget component as props.
- * This function is used only by the react-redux connect function.
- * @memberof UserWidget
- * @param {object} state - The Redux state
- * @returns {object} Redux state properties used in the UserWidget component
- */
-const mapStateToProps = (state) => {
-    return {
-        name: state.auth.usersName,
-        profilePictureUrl: state.auth.usersPictureUrl,
-    };
+            {/* User's name */}
+            <h2 className={styles.userWidgetName}>{name}</h2>
+        </section>
+    );
 };
 
-// Prop types for the UserWidget component
-UserWidget.propTypes = {
-    /**
-     * The user's name
-     */
-    name: PropTypes.string.isRequired,
-
-    /**
-     * The user's profile picture url
-     */
-    profilePictureUrl: PropTypes.string,
-};
-
-export default connect(mapStateToProps)(UserWidget);
+export default UserWidget;
